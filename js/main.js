d17094db4904fb8b50baf557cc4dce0a8e0fdd88
David Krumholz <davidkrumholz@yahoo.com.mx>
2023-07-29 12:26:16 -0600

    Create list of Posts main

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
-import { createHeader } from "./components/dom.js";
+import { createHeader, createPostCard } from "./components/dom.js";
+import { getAllPosts } from "./modules/postAPI.js";
+
 let header = document.getElementById("header");
 header.append(createHeader());
 
-// let card = createPostCard();
+let postsArray = [];
 
-// document.getElementById("wrapperPosts").append(card);
+const getPosts = async () => {
+    let data = await getAllPosts();
+    let keys = Object.keys(data);
+    let result = keys.reduce((accum, current) => {
+        return [...accum, {key: current, ...data[current] }]
+    }, []);
+    postsArray = result;
+    printAllCards(postsArray);
+}
+
+getPosts();
+
+const printAllCards = (postsList) => {
+   let postsWrapper = document.getElementById("wrapperPosts");
+   postsList.forEach((post, index) => {
+    let postCard = createPostCard(post, index);
+    postsWrapper.append(postCard);
+   });
+}
