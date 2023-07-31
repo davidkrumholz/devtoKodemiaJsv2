b2b79c75eba40768b200d880e01101669ca22b7a
MANOLOOG <101225098+MANOLOOG@users.noreply.github.com>
2023-07-31 16:19:53 -0600

    Adding feature post details

--- js/modules/postAPI.js
+++ js/modules/postAPI.js
@@ -1,11 +1,17 @@
 //Post API
 
-const BASE_URL = "https://equipo3js-f94ab-default-rtdb.firebaseio.com"
+const BASE_URL = "https://equipo3js-f94ab-default-rtdb.firebaseio.com";
 
 const getAllPosts = async () => {
-    let response = await fetch(`${BASE_URL}/posts/.json`);
-    let data = response.json();
-    return data;
-}
+  let response = await fetch(`${BASE_URL}/posts/.json`);
+  let data = response.json();
+  return data;
+};
 
-export { getAllPosts }
+const getDetailPost = async (postId) => {
+  let response = await fetch(`${BASE_URL}/posts/${postId}/.json`);
+  let data = response.json();
+  return data;
+};
+
+export { getAllPosts, getDetailPost, getCommentsData };
