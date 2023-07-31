cc7110f4b9cfe45a9b1274b4e1f5159c55ee9748
MANOLOOG <101225098+MANOLOOG@users.noreply.github.com>
2023-07-30 20:23:27 -0600

    Adding the login functionality

--- js/login.js
+++ js/login.js
@@ -1,1 +1,34 @@
 //login JS
+let user = {};
+
+let loginFields = document.querySelectorAll("#loginForm input");
+
+loginFields.forEach((field) => {
+  field.addEventListener("keyup", (event) => {
+    let property = event.target.name;
+    let value = event.target.value;
+    user[property] = value;
+  });
+});
+
+const saveUser = async (userObject) => {
+  let response = await fetch(
+    "https://equipo3js-f94ab-default-rtdb.firebaseio.com/users/.json",
+    { method: "POST", body: JSON.stringify(userObject) }
+  );
+
+  let data = await response.json();
+  return data;
+};
+
+document.getElementById("btnLogin").addEventListener("click", async () => {
+  let response = await saveUser(user);
+  console.log(response);
+  if (response.name) {
+    localStorage.setItem(
+      "token",
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+    );
+    window.open("../index.html", "_self");
+  }
+});
