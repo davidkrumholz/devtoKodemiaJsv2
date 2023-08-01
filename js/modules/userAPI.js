6bb9265409e4ff911a414a9fa8156e5eb67a740c
David Krumholz <davidkrumholz@yahoo.com.mx>
2023-08-01 12:54:16 -0600

    Add functionality create user to api user and something else

--- js/modules/userAPI.js
+++ js/modules/userAPI.js
@@ -1,1 +1,12 @@
 // userAPI
+const saveUser = async (userObject) => {
+    let response = await fetch(
+      "https://equipo3js-f94ab-default-rtdb.firebaseio.com/users/.json",
+      { method: "POST", body: JSON.stringify(userObject) }
+    );
+  
+    let data = await response.json();
+    return data;
+  };
+
+  export {saveUser}
